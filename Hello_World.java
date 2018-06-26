3a7c17c6bc6f1636aff4d6e2fbca2dd7e1deab17
1916378 <40574442+1916378@users.noreply.github.com>
2018-06-26 10:18:30 -0700

    Commited Hello World, First Assignment

---
+++ Hello_World.java
@@ -0,0 +1,7 @@
+public class Hello_World{
+ public static void main(String[] args){
+  //This is a print statement
+  System.out.print("Where is ny newline\n?");
+  System.out.println("Hello, World");
+ }
+}
