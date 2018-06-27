6a93a25410089e9edef57a04f3b72cda51747f0d
1916378 <40574442+1916378@users.noreply.github.com>
2018-06-26 17:04:31 -0700

    Printing Words, First Assignment (Real)

--- Hello_World.java
+++ Hello_World.java
@@ -1,7 +1,20 @@
 public class Hello_World{
  public static void main(String[] args){
   //This is a print statement
-  System.out.print("Where is ny newline\n?");
+  System.out.print("Where is ny newline?\n");
+  System.out.println("It's under here\tdown here");
   System.out.println("Hello, World");
+  System.out.println("\n\n\n\n\n\n\nLook at this");
+  System.out.println("    ___ ___               __       __            ");
+  System.out.println("   |     |     |   ____  |  |      |  |       _____  ");
+  System.out.println("   |            | |  _   | |  |      |  |      |   _    | ");
+  System.out.println("   |            | | ___| |  |__   |  |_    |  |_|  | ");
+  System.out.println("   |___|___| |____  |____| |____| |_____| ");
+  System.out.print("\nAnd this");
+  System.out.print("\n                        #       # ");
+  System.out.print("\n#   # ### ### #  ### ");
+  System.out.print("\n### #  # #      #   #  # ");
+  System.out.print("\n### ### #     ## ### ");
+  System.out.print("\n\nTHANK YOU");
  }
 }
