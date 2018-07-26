579e9622229abba8b0830c61fd57bccb59d289cd
1916378 <40574442+1916378@users.noreply.github.com>
2018-07-26 09:31:59 -0700

    Lesson 3

---
+++ Lesson 03/Lab03_06.java
@@ -0,0 +1,13 @@
+public class Lab03_06 {
+	public static void main(String[ ] args) {
+		int tablesize = 12; 
+		System.out.println("__________________");
+		for (int i = 1; i <= tablesize; i++){
+		System.out.print(" | ");
+			for(int j = 1; j <= tablesize; j++){
+			System.out.print((i*j) + " ");
+			}
+		System.out.println();
+		}
+	}
+}
