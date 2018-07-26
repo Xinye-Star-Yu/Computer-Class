6ab158887aac11a90427ad7933ef3e858b3c3fad
1916378 <40574442+1916378@users.noreply.github.com>
2018-07-26 09:07:00 -0700

    Lesson_3

--- Lesson 03/Lab03_05.java
+++ Lesson 03/Lab03_05.java
@@ -3,12 +3,21 @@ public class Lab03_05 {
 	public static void main(String[] args){
 		//Setup Scanner
 		Scanner userInput = new Scanner(System.in);
-		System.out.print("What number would you like the factorial of? :: ");
-		int num = userInput.nextInt();
-		int i,fac = 1; 
-		for (i = 1;i <= num; i++){
-		fac = fac * i;
+		System.out.println("Enter a coefficient integer? :: ");
+		int nValue = userInput.nextInt(); 
+		userInput.nextLine();
+		System.out.println("Enter the size of the table? :: ");
+		int tablesize = userInput.nextInt(); 
+		userInput.nextLine();
+		System.out.println("\nFormula: " + nValue + "x");
+		System.out.println("__________________");
+		for (int i = 1;i <(tablesize + 1); i++){
+			int temp = nValue * i;
+			format(i, temp);
 		}
-		System.out.print("The answer is " + fac);
+		System.out.println("__________________");
+	}
+	public static void format(int foo, int bar){
+		System.out.printf("| %3d |\t%5d\t|\n", foo, bar);
 	}
 }
