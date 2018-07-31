20a3ac26a4a77838bf903823c6f0930e2b67bad0
1916378 <40574442+1916378@users.noreply.github.com>
2018-07-31 09:46:20 -0700

    Discount Receipt

--- Lesson 03/DiscountReceipt.java
+++ Lesson 03/DiscountReceipt.java
@@ -1,42 +1,54 @@
-public class DiscountReceipt
-{
+import java.util.Scanner;
+public class DiscountReceipt {
 	public static void main(String[] args){
-		{
-			item computer = new item("computer" , 899.99 , (100000 + (int)(Math.random()*999999)));
-			System.out.println(computer.getObject());
-			System.out.println(computer.getPrice());
-			System.out.println(computer.getId());
-			double computerprice = 899.99;
-			
-			item monkey = new item("monkey" , 199.99 , (100000 + (int)(Math.random()*999999)));
-			System.out.println(monkey.getObject());
-			System.out.println(monkey.getPrice());
-			System.out.println(monkey.getId());
-			double monkeyprice = 199.99;
-			
-			item sandwich = new item("sandwich" , 9.99 , (100000 + (int)(Math.random()*999999)));
-			System.out.println(sandwich.getObject());
-			System.out.println(sandwich.getPrice());
-			System.out.println(sandwich.getId());
-			double sandwichprice = 9.99;
-			
-			item shoes = new item("shoes" , 19.99 , (100000 + (int)(Math.random()*999999)));
-			System.out.println(shoes.getObject());
-			System.out.println(shoes.getPrice());
-			System.out.println(shoes.getId());
-			double shoesprice = 19.99;
-
-			double subtotal;
-			if (computerprice + monkeyprice + sandwichprice + shoesprice > 2000){
-				double discount = 0.15;
-				subtotal = (computerprice + monkeyprice + sandwichprice + shoesprice) * discount;
-			}
-			else {
-				subtotal = computerprice + monkeyprice + sandwichprice + shoesprice;
-			}
-			double tax = 1.08;
-			System.out.println("Your total is ");
-			System.out.printf("%.2f",subtotal * tax);
+	//Setup Scanner
+	Scanner userInput = new Scanner(System.in);	
+		item[] shoppingList = new item[4];
+		double totalPrice = 0;
+		for(int i = 0; i < shoppingList.length; i++){
+			System.out.print("What item do you want to buy?:: ");
+			String itemName = userInput.nextLine();
+			System.out.print("What is the price?:: ");
+			double itemPrice = userInput.nextDouble();
+			int itemId = 100000 + (int)(Math.random()*999999);
+			shoppingList[i] = new item(itemName, itemPrice, itemId);
+			totalPrice += itemPrice;
+			userInput.nextLine();
 		}
+
+		double subtotal;
+		double discount = 0.85;
+		double tax = 1.08;	
+		if(calcDiscount(totalPrice)){
+			subtotal = discount * totalPrice;
+		}
+		else {
+			subtotal = totalPrice;
+		}
+		printreceipt(shoppingList, subtotal, tax, discount, totalPrice);
+		
+	}
+			
+	public static boolean calcDiscount(double totalPrice){
+		if (totalPrice > 2000){
+			return true;
+			}
+			return false;
+		}
+			
+	public static void printreceipt(item[] shoppingList, double subtotal, double tax, double discount, double totalPrice){
+		System.out.println("---------------");
+		System.out.println("----Receipt----");
+		System.out.println("---------------");
+		for (int i = 0; i < shoppingList.length; i++){
+			System.out.println(shoppingList[i].toString());
+		}
+		System.out.println("Subtotal :: " + totalPrice);
+		if (calcDiscount(totalPrice)){
+			subtotal *= 0.85;
+			System.out.printf("\nDiscount(15%%):: " + "%.2f", subtotal);
+		}
+		System.out.printf("\nTax(8%%):: " + "%.2f", totalPrice * 0.08);
+		System.out.printf("\nTotal:: " + "%.2f", subtotal * tax);	
 	}
 }
